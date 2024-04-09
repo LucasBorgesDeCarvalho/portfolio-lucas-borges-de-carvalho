5ccffe4037182b2796dc06684a05b0f6ae8ae68a
LucasBorgesDeCarvalho <lucas.carvalho30111@gmail.com>
2024-04-09 08:41:39 -0300

    Update About

--- components/About.jsx
+++ components/About.jsx
@@ -44,6 +44,11 @@ const qualificationData = [
         title: 'education',
         data: [
             {
+                university: 'Sesi Djalma Pessoa',
+                qualification: 'School',
+                years: '2018 - 2020',
+            },
+            {
                 university: 'Centro Universitário Jorge Amado - (UNIJORGE)',
                 qualification: 'Computer Science Undergraduate',
                 years: '2021 - 2024',
@@ -56,7 +61,7 @@ const qualificationData = [
             {
                 university: 'Kumon',
                 qualification: 'Inglês',
-                years: '2018 - 2021',
+                years: '2017 - 2020',
             },
         ],
     },
@@ -65,8 +70,13 @@ const qualificationData = [
         data: [
             {
                 company: 'Centro Universitário Jorge Amado - (UNIJORGE)',
-                qualification: 'Web Development Tutor',
+                role: 'Web Development Tutor',
                 years: '2024 - ',
+            },
+            {
+                company: 'Sesi Djalma Pessoa (Robotic Team)',
+                role: 'Manufacturing/Production Engineer',
+                years: '2020 - 2020',
             },
         ],
     },
@@ -142,10 +152,95 @@ const About = () => {
                                     <div className="text-center xl:text-left">
                                         <h3 className="h3 mb-4">Estudante de Ciências da Computação com Foco em Desenvolvimento Web</h3>
                                         <p className="subtitle max-w-xl mx-auto xl:mx-0">Estudante na UNIJORGE desde março de 2018, dedicado ao aprimoramento em linguagens de programação, com foco atual em React, Node.js e JavaScript. Busca constante melhoria técnica, aplicando conhecimentos em projetos pessoais e acadêmicos, e está aberto a explorar novas tecnologias para consolidar seu aprendizado.</p>
+
+                                        {/*Icons*/}
+                                        <div className="grid xl:grid-cols-2 gap-4 mb-12">
+                                            {infoData.map((item, index) => {
+                                                return (
+                                                    <div className="flex items-center gap-x-4 mx-auto xl:mx-0" key={index}>
+                                                        <div className="text-primary">{item.icon}</div>
+                                                        <div>{item.text}</div>
+                                                    </div>
+                                                );
+                                        })}
+                                        </div>
+                                        {/*Languages*/}
+                                        <div className='flex flex-col gap-y-2'>
+                                            <div className='text-primary'>Language Skills</div>
+                                            <div className='border-b border-border'></div>
+                                            <div>English, Spanish, Portuguese</div>
+                                        </div>
                                     </div>
                                 </TabsContent>
-                                <TabsContent value='qualifications'>Qualification info</TabsContent>
-                                <TabsContent value='skills'>Skills info</TabsContent>
+                                {/*Qualifications*/}
+                                <TabsContent value='qualifications'>
+                                    <div>
+                                        <h3 className="h3 mb-8 text-center xl:text-left">My Awesome Journey</h3>
+                                        {/*Experience & education wrapper*/}
+                                        <div className="grid md:grid-cols-2 gap-y-8">
+                                            {/*Experience*/}
+                                            <div className="flex flex-col gap-y-6">
+                                                <div className="flex gap-x-4 items-center text-[22px] text-primary">
+                                                    <Briefcase size={28}/>
+                                                    <h4 className="capitalize font-medium">
+                                                        {getData(qualificationData, 'experience').title}
+                                                    </h4>
+                                                </div>
+                                                {/*List*/}
+                                                <div className="flex flex-col gap-y-8">
+                                                    {getData(qualificationData, 'experience').data.map((item, index) => {
+                                                        const {company, role, years} = item; 
+                                                        return (
+                                                            <div className="flex gap-x-8 group" key={index}>
+                                                                <div className="h-[84px] w-[1px] bg-border relative ml-2">
+                                                                    <div className="w-[11px] h-[11px] rounded-full bg-primary absolute -left-[5px] group-hover:translate-y-[84px] transition-all duration-500"></div>
+                                                                </div>  
+                                                                <div>
+                                                                    <div className="font-semibold text-xl leading-none mb-2">{company}</div>
+                                                                    <div className="text-lg leading-none text-muted-foreground mb-4">{role}</div>
+                                                                    <div className="text-base font-medium">{years}</div>
+                                                                </div>
+                                                            </div>
+                                                        )
+                                                    })}
+                                                </div>
+                                            </div>
+                                            {/*Education*/}
+                                            <div className="flex flex-col gap-y-6">
+                                                <div className="flex gap-x-4 items-center text-[22px] text-primary">
+                                                    <GraduationCap size={28}/>
+                                                    <h4 className="capitalize font-medium">
+                                                        {getData(qualificationData, 'education').title}
+                                                    </h4>
+                                                </div>
+                                                {/*List*/}
+                                                <div className="flex flex-col gap-y-8">
+                                                    {getData(qualificationData, 'education').data.map((item, index) => {
+                                                        const {university, qualification, years} = item; 
+                                                        return (
+                                                            <div className="flex gap-x-8 group" key={index}>
+                                                                <div className="h-[84px] w-[1px] bg-border relative ml-2">
+                                                                    <div className="w-[11px] h-[11px] rounded-full bg-primary absolute -left-[5px] group-hover:translate-y-[84px] transition-all duration-500"></div>
+                                                                </div>  
+                                                                <div>
+                                                                    <div className="font-semibold text-xl leading-none mb-2">{university}</div>
+                                                                    <div className="text-lg leading-none text-muted-foreground mb-4">{qualification}</div>
+                                                                    <div className="text-base font-medium">{years}</div>
+                                                                </div>
+                                                            </div>
+                                                        )
+                                                    })}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </TabsContent>
+                                {/*Skills*/}
+                                <TabsContent value='skills'>
+                                    <div className="text-center xl:text-left">
+                                        <h3 className="h3 mb-8">Tools I Use Everyday</h3>
+                                    </div>
+                                </TabsContent>
                             </div>
                         </Tabs>
                     </div>

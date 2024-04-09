72c9ca56d35b40d95c0345e22f895e2d69d93ad9
LucasBorgesDeCarvalho <lucas.carvalho30111@gmail.com>
2024-04-09 11:38:42 -0300

    Fix About CSS

--- components/About.jsx
+++ components/About.jsx
@@ -131,7 +131,7 @@ const About = () => {
     return (
         <section className="xl:h-[860px] pb-12 xl:py-24">
             <div className="container mx-auto">
-                <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">About me</h2>
+                <h2 className="section-title mt-40 sm:mt-24 mb-8 xl:mb-16 text-center mx-auto">About me</h2>
                 <div className='flex flex-col xl:flex-row'>
                     {/*Image*/}
                     <div className='hidden xl:flex flex-1 relative'>
